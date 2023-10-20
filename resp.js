589a38d49ef8de21a84edd6d7348c3aa88d13cd0
TechEnthusiast7 <146336888+TechEnthusiast7@users.noreply.github.com>
2023-10-20 16:57:57 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,10 @@
+burger = document.querySelector("burger")
+navbar = document.querySelector("navbar")
+rightnav = document.querySelector("rightnav")
+navlist = document.querySelector("navlist")
+
+burger.addEventListener('click', () => {
+    rightnav.classList.toggle('vclass');
+    navlist.classList.toggle('vclass');
+    navbar.classList.toggle('hnav');
+});
